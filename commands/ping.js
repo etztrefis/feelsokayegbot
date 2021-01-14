0c9aa8490a425d297772d18894d8f652594102e5
etztrefis <etztrefishello@gmail.com>
2021-01-14 20:34:05 +0700

    commands folder and first ping command init

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+module.exports.help = {
+    name: 'ping',
+    archived: false,
+    cooldown: 5000,
+    cooldown_mode: 'UserCommand',
+    aliases: ['pang', 'peng', 'pong', 'pung', 'pyng', 'ping'],
+    description: 'Ping!',
+    run: async () => {
+        const channelsCount = await fob.Channel.getJoinable();
+        return `Pong! Okayeg \\u{1F44D} Channels: ${channelsCount.length} Uptime: ${fob.Utils.misc.uptime()}.`;
+    },
+};
