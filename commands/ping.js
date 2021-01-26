da681a2aa4fdb8f4b7d64d4df545b3807f09dcaa
etztrefis <etztrefishello@gmail.com>
2021-01-26 15:20:51 +0700

    huh

--- commands/ping.js
+++ commands/ping.js
@@ -8,6 +8,6 @@ module.exports.help = {
     description: 'Ping!',
     run: async (context) => {
         const channelsCount = await fob.Channel.getJoinable();
-        return `@${context.user.name}, Pong! Okaye  g \\u{1F44D} Channels: ${channelsCount.length}, Commands executed: ${fob.Temp.cmdCount}, Uptime: ${fob.Utils.misc.uptime()}.`;
+        return `@${context.user.name}, Pong! Okayeg \\u{1F44D} Channels: ${channelsCount.length}, Commands executed: ${fob.Temp.cmdCount}, Uptime: ${fob.Utils.misc.uptime()}.`;
     },
 };
