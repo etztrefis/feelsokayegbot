103bf3f31cc667db3ecb6ad7073a25a144325427
etztrefis <etztrefishello@gmail.com>
2021-01-29 19:38:13 +0300

    added temperature arument into ping response

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,15 @@ module.exports.help = {
     aliases: ['pang', 'peng', 'pong', 'pung', 'pyng', 'ping'],
     description: 'Ping!',
     run: async (context) => {
+        const {promisify} = require('util');
+        const exec = promisify(require('child_process').exec);
+        const readFile = require('fs').promises.readFile;
+        const [temperature, memory] = await Promise.all([
+            exec('sensors'),
+            readFile('/proc/meminfo'),
+        ]);
         const channelsCount = await fob.Channel.getJoinable();
-        return `@${context.user.name}, Pong! Okayeg \\u{1F44D} Channels: ${channelsCount.length}, Commands executed: ${fob.Temp.cmdCount}, Uptime: ${fob.Utils.misc.uptime()}.`;
+        return `@${context.user.name}, Pong! Okayeg \\u{1F44D} Channels: ${channelsCount.length}, Commands executed: ${fob.Temp.cmdCount}, Uptime: ${fob.Utils.misc.uptime()},
+            Temperature: ${temperature.stdout.match(/[+]..../)[0].replace('+', '')}°C.`;
     },
 };
