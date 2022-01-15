7f9de78b428a507c76158ebe062579ccc889d1fc
trefis <trefis@pm.me>
2022-01-15 18:27:30 +0700

    🛠️ utils folder now includes db, misc, logger

---
+++ utils/misc.ts
@@ -0,0 +1,50 @@
+import * as humanize from "humanize-duration";
+import { okayeg } from "..";
+
+const shortHumanize = humanize.humanizer({
+  language: "shortEn",
+  languages: {
+    shortEn: {
+      y: () => "y",
+      mo: () => "mo",
+      w: () => "w",
+      d: () => "d",
+      h: () => "h",
+      m: () => "m",
+      s: () => "s",
+      ms: () => "ms",
+    },
+  },
+});
+
+const uptime = () => {
+  const ms = process.uptime() * 100;
+  return shortHumanize(ms, {
+    units: ["w", "d", "h", "m", "s"],
+    largest: 4,
+    round: true,
+    conjunction: "",
+    spacer: "",
+  });
+};
+
+const logError = async (name: string, reason: string, stack: string) => {
+  await okayeg.Utils.db.log.create({
+    data: {
+      name: name,
+      message: reason,
+      stack: stack,
+    },
+  });
+};
+
+const updateBannedState = async (channelId: string, isBanned: boolean) => {
+  await okayeg.Utils.db.channel.update({
+    where: { userId: channelId },
+    data: {
+      ignore: isBanned,
+    },
+  });
+};
+
+export { uptime, logError, updateBannedState };
---
+++ utils/winston.ts
@@ -0,0 +1,56 @@
+import { createLogger, format, transports, addColors } from "winston";
+import * as pc from "picocolors";
+
+const { combine, colorize, timestamp, printf } = format;
+
+const loggerLevels = {
+  colors: {
+    info: "green",
+    error: "underline bold red",
+    debug: "bold magenta",
+    warn: "yellow",
+  },
+};
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${pc.magenta(timestamp)} | [${level}]: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(
+    format((info) => {
+      info.level = info.level.toUpperCase();
+      return info;
+    })(),
+    timestamp({
+      format: "DD.MM.YY HH:mm:ss",
+    }),
+    colorize(),
+    logFormat
+  ),
+  transports: [
+    new transports.Console({
+      stderrLevels: ["error"],
+    }),
+  ],
+});
+
+addColors(loggerLevels.colors);
+
+logger.transports[0].level = "info";
+logger.info(`Setting log level to ${logger.transports[0].level}`);
+
+const info = (message: string) => {
+  logger.info(message);
+};
+const error = (message: string) => {
+  logger.error(message);
+};
+const warn = (message: string) => {
+  logger.warn(message);
+};
+const debug = (message: string) => {
+  logger.debug(message);
+};
+
+export { info, error, warn, debug };
