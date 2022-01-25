97283a8d2ff3a56f61be2c60bfa408576475922a
trefis <trefis@pm.me>
2022-01-25 21:30:06 +0700

    🔃 reload commands command

---
+++ commands/update.ts
@@ -0,0 +1,34 @@
+import { Bot, cmdData } from "../types";
+import * as pc from "picocolors";
+import { levels } from "../modules/cooldowns";
+
+export const command = {
+  name: "update",
+  active: true,
+  cooldown_mode: levels.DISABLED,
+  author_permission: true,
+  aliases: [],
+  run: async (context: cmdData, okayeg: Bot) => {
+    okayeg.Logger.info(
+      `${pc.green(
+        "[UPDATE]"
+      )} || Called update command. Resynchronizing commands.`
+    );
+
+    await okayeg.Utils.loadCommands()
+      .then(async () => {
+        await okayeg.CommandUtils.send(
+          context.channel,
+          "Resynchronizing commands... 🤔 🔍 ",
+          context
+        );
+      })
+      .finally(async () => {
+        await okayeg.CommandUtils.send(
+          context.channel,
+          "Now bot is up to date Okayeg 👍",
+          context
+        );
+      });
+  },
+};
