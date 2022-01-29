3211b7449e096e1f4ca8cfc41e52caaea98bbc30
etztrefis <trefis@pm.me>
2022-01-29 16:03:00 +0700

    🎵 add spotify api clients

--- utils/apiClients.ts
+++ utils/apiClients.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
+import * as qs from "querystring";
 import { okayeg } from "..";
 
 // Generic GET requests
@@ -43,6 +44,42 @@ export const tmi = async (link: string, config?: AxiosRequestConfig) => {
   const { data } = await axios.request({
     url: `https://tmi.twitch.tv/${link}`,
     timeout: 3500,
+    ...config,
+  });
+  return data;
+};
+
+// Spotify access token
+const spotifyAccess = async (config?: AxiosRequestConfig) => {
+  const basic = Buffer.from(
+    `${okayeg.Config.spotifyClient}:${okayeg.Config.spotifySecret}`
+  ).toString("base64");
+  const { data } = await axios.request({
+    url: `https://accounts.spotify.com/api/token`,
+    timeout: 1500,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${basic}`,
+    },
+    data: qs.stringify({
+      grant_type: "refresh_token",
+      refresh_token: okayeg.Config.spotifyRefresh,
+    }),
+    ...config,
+  });
+  return data;
+};
+
+// Spotify API
+export const spotify = async (link: string, config?: AxiosRequestConfig) => {
+  const { access_token } = await spotifyAccess();
+  const { data } = await axios.request({
+    url: `https://api.spotify.com/v1/me/${link}`,
+    timeout: 1500,
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
     ...config,
   });
   return data;
